2011a6b9ea79f808e5af8277c05cf2f8d377cf4f
Ahmet kocabaş <ahmetpos3@gmail.com>
2022-05-14 12:01:49 +0300

    datalari sifrelemek ve cozmek icin crypto kutuphanesi kullanmaya karar verdik

---
+++ key.py
@@ -0,0 +1,20 @@
+from Crypto import Random
+from Crypto.PublicKey import RSA
+import base64
+
+def generate_keys():
+	modulus_length = 256*4
+    privatekey = RSA.generate(modulus_length, Random.new().read)
+	publickey = privatekey.publickey()
+
+	return privatekey, publickey
+
+def veri_sifrele(data , publickey):
+	encrypted_data = publickey.encrypt(data, 32)[0]
+	encoded_encrypted_data = base64.b64encode(encrypted_data)
+	return encoded_encrypted_data
+
+def veri_coz(encoded_encrypted_data, privatekey):
+	decoded_encrypted_data = base64.b64decode(encoded_encrypted_data)
+	decoded_decrypted_data = privatekey.decrypt(decoded_encrypted_data)
+	return decoded_decrypted_data
